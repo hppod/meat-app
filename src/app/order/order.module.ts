ebe4eb776d2a7d1dd06ab2d7ad70c521735ad230
hppod <hppodgornik@gmail.com>
2018-10-08 10:31:02 -0300

    aplicação modularizada

---
+++ src/app/order/order.module.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
+import { NgModule } from "@angular/core";
+import { OrderComponent } from "./order.component";
+import { SharedModule } from '../shared/shared.module';
+
+const ROUTES: Routes = [
+    { path: '', component: OrderComponent }
+]
+
+@NgModule({
+    declarations: [
+        OrderComponent,
+        OrderItemsComponent,
+        DeliveryCostsComponent
+    ],
+    imports: [
+        SharedModule,
+        RouterModule.forChild(ROUTES)
+    ]
+})
+export class OrderModule {
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,42 @@
+import { OrderService } from './../order/order.service';
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from '../restaurants/restaurant/restaurants.service';
+
+@NgModule({
+    declarations: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
+})
+export class SharedModule { 
+    static forRoot(): ModuleWithProviders{
+        return{
+            ngModule: SharedModule,
+            providers: [
+                OrderService,
+                ShoppingCartService,
+                RestaurantsService
+            ]
+        }
+    }
+}
