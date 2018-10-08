80879f61dce68a2630ded7b89e06cc1b316ff7bd
hppod <hppodgornik@gmail.com>
2018-10-08 07:36:26 -0300

    primeiro commit, aplicação funcionando com template forms

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { OrderComponent } from './order/order.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    },
+    { path: 'order', component: OrderComponent },
+    { path: 'order-summary', component: OrderSummaryComponent }
+
+]
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,57 @@
+import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
+import { OrderService } from './order.service';
+import { RadioOption } from './../shared/radio/radio-option.model';
+import { Component, OnInit } from '@angular/core';
+import { Order, OrderItem } from './order.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  delivery: number = 8
+
+  paymentOptions: RadioOption[] = [
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Refeição', value: 'REF' }
+  ]
+
+  constructor(private orderService: OrderService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue()
+  }
+
+  cartItems() {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem) {
+    return this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem) {
+    return this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)
+      .subscribe((orderId: string) => {
+        this.router.navigate(['/order-summary'])
+        this.orderService.clear()
+      })
+    console.log(order)
+  }
+}
