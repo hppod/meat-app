de5bd29a41b468cc04f65fafd0b0d11e7c36d913
hppod <hppodgornik@gmail.com>
2018-11-18 20:24:52 -0200

    adiciona zona ao error handler

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,14 +1,14 @@
 import { Injector } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable';
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, NgZone } from '@angular/core';
 import { NotificationService } from './shared/messages/notification.service';
 import { LoginService } from './security/login/login.service';
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private ns: NotificationService, private injector: Injector) {
+    constructor(private ns: NotificationService, private injector: Injector, private zone: NgZone) {
         super()
     }
 
@@ -16,17 +16,19 @@ export class ApplicationErrorHandler extends ErrorHandler {
 
         if (errorResponse instanceof HttpErrorResponse) {
             const message = errorResponse.error.message
-            switch (errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin()
-                    break;
-                case 403:
-                    this.ns.notify(message || 'Não autorizado')
-                    break;
-                case 404:
-                    this.ns.notify(message || 'Não encontrado.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin()
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado')
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Não encontrado.')
+                        break;
+                }
+            })
         }
         super.handleError(errorResponse)
     }
