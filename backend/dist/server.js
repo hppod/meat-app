938b31e5b000cf41921ddd90cad834d76471e5a6
hppod <hppodgornik@gmail.com>
2018-11-18 00:25:09 -0200

    script do servidor backend rodando em https

---
+++ backend/dist/server.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+var fs = require("fs");
+var https = require("https");
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+var port = 3001;
+var API = "https://localhost:" + port;
+https.createServer(options, server).listen(port, function () {
+    console.log("Servidor rodando em " + API);
+});
---
+++ backend/server.ts
@@ -0,0 +1,26 @@
+import * as jsonServer from 'json-server';
+import { Express } from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+
+const server: Express = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+server.use(router);
+
+const options = {
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+};
+
+const port = 3001;
+const API = `https://localhost:${port}`;
+
+https.createServer(options, server).listen(port, () => {
+  console.log(`Servidor rodando em ${API}`);
+});
